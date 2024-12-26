bc5d0fcfbfb9eed6fcb88036c2fdd44f17a84bd3
johnjones751 <johnjonesjr1121@gmail.com>
2024-12-26 16:15:11 -0600

    final commit

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ function calculateTemp(c) {
 }
 let userInput = prompt("What is the temp in C?");
 calculateTemp(userInput);
+
+function determineAgeGroup(age) {
+  if (age < 18) {
+    alert("Minor");
+  } else if (age >= 18 && age < 62) {
+    alert("Adult");
+  } else {
+    alert("Senior");
+  }
+}
+userInput = prompt("What is your age?");
+determineAgeGroup(userInput);
+
+function simpleMathCalculator(num1, num2, operator) {
+  //addition, subtraction, multiplication, division;
+  if (operator === "addition") {
+    alert(Number(num1) + Number(num2));
+  } else if (operator === "subtraction") {
+    alert(num1 - num2);
+  } else if (operator === "multiplication") {
+    alert(num1 * num2);
+  } else if (operator === "division") {
+    alert(num1 / num2);
+  }
+}
+let number1 = prompt("Please enter first number");
+let number2 = prompt("Please enter second number");
+let operator = prompt(
+  "What operation will be used here? eg. addition, subtraction, multiplication, division"
+);
+simpleMathCalculator(number1, number2, operator);
